55a081209eae6282929de6302c44706a1afd4ed9
lat9 <lat9@vinosdefrutastropicales.com>
2025-02-21 10:01:00 -0500

    Remove all but zc155 changes
    ... with those changes moved to the repository's root

--- ZenCart_1.5.4/includes/extra_datafiles/referral_database_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-// referrals mod
-
-  if (!defined('DB_PREFIX')) define('DB_PREFIX', '');
-  define('TABLE_SOURCES', DB_PREFIX . 'sources');//rmh referral
-  define('TABLE_SOURCES_OTHER', DB_PREFIX . 'sources_other');//rmh referral
-
--- ZenCart_1.5.5/admin/includes/extra_datafiles/referral_database_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-// referrals mod
-
-  if (!defined('DB_PREFIX')) define('DB_PREFIX', '');
-  define('TABLE_SOURCES', DB_PREFIX . 'sources');
-  define('TABLE_SOURCES_OTHER', DB_PREFIX . 'sources_other');
